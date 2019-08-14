d7fd6ff1f2f78dfe763d4268c22ad17f1217aa80
keep2iron <keep2iron@gmail.com>
2019-08-14 11:48:02 +0800

    1.modify buildTools version

--- app/src/main/java/io/github/keep2iron/app/MainActivity.kt
+++ app/src/main/java/io/github/keep2iron/app/MainActivity.kt
@@ -1,8 +1,14 @@
 package io.github.keep2iron.app
 
+import android.graphics.Color
+import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.BackgroundColorSpan
+import android.text.style.DynamicDrawableSpan
+import android.text.style.ImageSpan
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import io.github.keep2iron.peach.DrawableCreator
@@ -14,21 +20,35 @@ class MainActivity : AppCompatActivity() {
     setContentView(R.layout.activity_main)
     val textView = findViewById<TextView>(R.id.textView)
 
-    textView.background = DrawableCreator()
-      .rectangle()
-      .complete()
-      .solidColor(ContextCompat.getColor(applicationContext, R.color.colorPrimary))
-      .complete()
-      .cornerRadius(10)
-      .complete()
-      .solidColorPressed(ContextCompat.getColor(applicationContext, R.color.colorAccent))
-      .complete()
-      .ripple(true)
-      .complete()
-      .build()
-    textView.setOnClickListener {
-      Toast.makeText(applicationContext, "click", Toast.LENGTH_SHORT).show()
-    }
+    val spannedString = SpannableString("自营商品")
+    val draw = GradientDrawable()
+    draw.setColor(Color.parseColor("#FFCCCCCC"))
+    draw.shape = GradientDrawable.RECTANGLE
+//    ImageSpan(
+//      DrawableCreator()
+//        .rectangle().complete().solidColor(Color.parseColor("#FFCCCCCC")).complete()
+//        .build(), DynamicDrawableSpan.ALIGN_BASELINE
+//    )
+    val drawable = getResources().getDrawable(R.mipmap.ic_launcher)
+    val imageSpan = ImageSpan(drawable, ImageSpan.ALIGN_BASELINE)
+    spannedString.setSpan(
+      imageSpan,0, 2, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+    )
+
+
+    textView.text = spannedString
+//    textView.background = DrawableCreator()
+//      .rectangle()
+//      .complete()
+//      .solidColor(ContextCompat.getColor(applicationContext, R.color.colorPrimary))
+//      .complete()
+//      .cornerRadius(10)
+//      .complete()
+//      .solidColorPressed(ContextCompat.getColor(applicationContext, R.color.colorAccent))
+//      .complete()
+//      .ripple(true)
+//      .complete()
+//      .build()
   }
 
 }
