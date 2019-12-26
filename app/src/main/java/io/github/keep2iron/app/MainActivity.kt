433c18e195058916ad18ce5b7e4aa58b6c81bcdf
keep2iron <497881309@qq.com>
2019-12-26 17:07:16 +0800

    1.删除报错堆栈信息 2.同步更新版本

--- app/src/main/java/io/github/keep2iron/app/MainActivity.kt
+++ app/src/main/java/io/github/keep2iron/app/MainActivity.kt
@@ -15,40 +15,49 @@ import io.github.keep2iron.peach.DrawableCreator
 
 class MainActivity : AppCompatActivity() {
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
-    val textView = findViewById<TextView>(R.id.textView)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val textView = findViewById<TextView>(R.id.textView)
 
-    val spannedString = SpannableString("自营商品")
-    val draw = GradientDrawable()
-    draw.setColor(Color.parseColor("#FFCCCCCC"))
-    draw.shape = GradientDrawable.RECTANGLE
+        val spannedString = SpannableString("自营商品")
+        val draw = GradientDrawable()
+        draw.setColor(Color.parseColor("#FFCCCCCC"))
+        draw.shape = GradientDrawable.RECTANGLE
 //    ImageSpan(
 //      DrawableCreator()
 //        .rectangle().complete().solidColor(Color.parseColor("#FFCCCCCC")).complete()
 //        .build(), DynamicDrawableSpan.ALIGN_BASELINE
 //    )
-    val drawable = getResources().getDrawable(R.mipmap.ic_launcher)
-    val imageSpan = ImageSpan(drawable, ImageSpan.ALIGN_BASELINE)
-    spannedString.setSpan(
-      imageSpan,0, 2, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-    )
+        val drawable = getResources().getDrawable(R.mipmap.ic_launcher)
+        val imageSpan = ImageSpan(drawable, ImageSpan.ALIGN_BASELINE)
+        spannedString.setSpan(
+                imageSpan, 0, 2, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
 
 
-    textView.text = spannedString
-//    textView.background = DrawableCreator()
-//      .rectangle()
-//      .complete()
-//      .solidColor(ContextCompat.getColor(applicationContext, R.color.colorPrimary))
-//      .complete()
-//      .cornerRadius(10)
-//      .complete()
-//      .solidColorPressed(ContextCompat.getColor(applicationContext, R.color.colorAccent))
-//      .complete()
-//      .ripple(true)
-//      .complete()
-//      .build()
-  }
+        textView.text = spannedString
+        textView.background = DrawableCreator()
+                .rectangle()
+                .complete()
+                .gradient()
+                .linearGradient()
+                .startColor(Color.RED)
+                .endColor(Color.BLUE)
+                .angle(45)
+                .complete()
+                .rounded()
+//                .topLeftRadius(60)
+//                .topRightRadius(60)
+                .complete()
+                .solidColorPressed(ContextCompat.getColor(applicationContext, R.color.colorAccent))
+                .complete()
+                .ripple(true)
+                .complete()
+                .build()
+        textView.setOnClickListener {
+
+        }
+    }
 
 }
--- lib-peach/src/main/java/io/github/keep2iron/peach/drawable/WrapperGradientDrawable.kt
+++ lib-peach/src/main/java/io/github/keep2iron/peach/drawable/WrapperGradientDrawable.kt
@@ -1,6 +1,18 @@
 package io.github.keep2iron.peach.drawable
 
+import android.graphics.Rect
 import android.graphics.drawable.GradientDrawable
+import kotlin.math.min
 
 class WrapperGradientDrawable : GradientDrawable() {
+
+    var radiusAdjustBounds: Boolean = false
+
+    override fun onBoundsChange(r: Rect) {
+        super.onBoundsChange(r)
+        if (radiusAdjustBounds) { // 修改圆角为短边的一半
+            cornerRadius = min(r.width(), r.height()) / 2.toFloat()
+        }
+    }
+
 }
