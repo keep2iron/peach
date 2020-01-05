c0774706470a62f969c36672ba018a386c52066b
keep2iron <497881309@qq.com>
2020-01-05 21:32:30 +0800

    1.添加新的submodule依赖

--- app/src/main/java/io/github/keep2iron/app/MainActivity.kt
+++ app/src/main/java/io/github/keep2iron/app/MainActivity.kt
@@ -8,6 +8,8 @@ import android.text.SpannableString
 import android.text.style.ImageSpan
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import io.github.keep2iron.peach.Peach
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,35 +18,37 @@ class MainActivity : AppCompatActivity() {
     setContentView(R.layout.activity_main)
     val textView = findViewById<TextView>(R.id.textView)
 
-    val spannedString = SpannableString("自营商品")
-    val draw = GradientDrawable()
-    draw.setColor(Color.parseColor("#FFCCCCCC"))
-    draw.shape = GradientDrawable.RECTANGLE
-//    ImageSpan(
-//      Peach()
-//        .rectangle().complete().solidColor(Color.parseColor("#FFCCCCCC")).complete()
-//        .build(), DynamicDrawableSpan.ALIGN_BASELINE
-//    )
-    val drawable = getResources().getDrawable(R.mipmap.ic_launcher)
-    val imageSpan = ImageSpan(drawable, ImageSpan.ALIGN_BASELINE)
-    spannedString.setSpan(
-      imageSpan,0, 2, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
-    )
-
-
-    textView.text = spannedString
-//    textView.background = Peach()
-//      .rectangle()
-//      .complete()
-//      .solidColor(ContextCompat.getColor(applicationContext, R.color.colorPrimary))
-//      .complete()
-//      .cornerRadius(10)
-//      .complete()
-//      .solidColorPressed(ContextCompat.getColor(applicationContext, R.color.colorAccent))
-//      .complete()
-//      .ripple(true)
-//      .complete()
-//      .build()
+//     val spannedString = SpannableString("自营商品")
+//     val draw = GradientDrawable()
+//     draw.setColor(Color.parseColor("#FFCCCCCC"))
+//     draw.shape = GradientDrawable.RECTANGLE
+// //    ImageSpan(
+// //      Peach()
+// //        .rectangle().complete().solidColor(Color.parseColor("#FFCCCCCC")).complete()
+// //        .build(), DynamicDrawableSpan.ALIGN_BASELINE
+// //    )
+//     val drawable = getResources().getDrawable(R.mipmap.ic_launcher)
+//     val imageSpan = ImageSpan(drawable, ImageSpan.ALIGN_BASELINE)
+//     spannedString.setSpan(
+//       imageSpan,0, 2, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+//     )
+//
+//
+//     textView.text = spannedString
+   textView.background = Peach()
+     .rectangle()
+     .complete()
+     .gradient()
+     .startColor(ContextCompat.getColor(applicationContext, R.color.colorPrimary))
+     .endColor(ContextCompat.getColor(applicationContext, R.color.colorPrimary))
+     .complete()
+     .cornerRadius(10)
+     .complete()
+     .solidColorPressed(ContextCompat.getColor(applicationContext, R.color.colorAccent))
+     .complete()
+     .ripple(true)
+     .complete()
+     .build()
   }
 
 }
